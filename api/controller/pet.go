233b0638397283f1bfa509fd9317604fa6cd2d29
rare0b <rare0b0110@gmail.com>
2023-10-20 12:56:17 +0900

    feat: petControllerの実装

--- api/controller/pet.go
+++ api/controller/pet.go
@@ -2,16 +2,19 @@ package controller
 
 import (
 	"encoding/json"
+	"github.com/go-chi/chi/v5"
 	"github.com/rare0b/go-pet-api/api/domain/entity"
 	"github.com/rare0b/go-pet-api/api/usecase"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type PetController interface {
 	UploadImage(w http.ResponseWriter, r *http.Request)
 	CreatePet(w http.ResponseWriter, r *http.Request)
 	UpdatePet(w http.ResponseWriter, r *http.Request)
-	GetPetsByStatus(w http.ResponseWriter, r *http.Request)
+	GetPetsByStatuses(w http.ResponseWriter, r *http.Request)
 	GetPetByID(w http.ResponseWriter, r *http.Request)
 	UpdatePetByID(w http.ResponseWriter, r *http.Request)
 	DeletePetByID(w http.ResponseWriter, r *http.Request)
@@ -84,17 +87,100 @@ func (c *petController) UpdatePet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (c *petController) GetPetsByStatus(w http.ResponseWriter, r *http.Request) {
-	//TODO: query param
+	queryStatus := r.URL.Query().Get("status")
+	if queryStatus == "" {
+		http.Error(w, `{"error": "Status parameter is required"}`, http.StatusBadRequest)
+		return
+	}
+
+	statuses := strings.Split(queryStatus, ",")
+
+	pets, err := c.petUsecase.GetPetsByStatuses(statuses)
+	if err != nil {
+		http.Error(w, `{"error": "Internal Server Error"}`, http.StatusInternalServerError)
+		return
+	}
+
+	response, err := json.Marshal(pets)
+	if err != nil {
+		http.Error(w, `{"error": "Internal Server Error"}`, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
 }
 
 func (c *petController) GetPetByID(w http.ResponseWriter, r *http.Request) {
-	//TODO: path param
+	petID, err := strconv.ParseInt(chi.URLParam(r, "petID"), 10, 64)
+	if err != nil {
+		http.Error(w, `{"error": "Invalid petID format"}`, http.StatusBadRequest)
+		return
+	}
+
+	pet, err := c.petUsecase.GetPetByID(petID)
+	if err != nil {
+		http.Error(w, `{"error": "Internal Server Error"}`, http.StatusInternalServerError)
+		return
+	}
+
+	response, err := json.Marshal(pet)
+	if err != nil {
+		http.Error(w, `{"error": "Internal Server Error"}`, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
 }
 
 func (c *petController) UpdatePetByID(w http.ResponseWriter, r *http.Request) {
-	//TODO: path param
+	petID, err := strconv.ParseInt(chi.URLParam(r, "petID"), 10, 64)
+	if err != nil {
+		http.Error(w, `{"error": "Invalid petID format"}`, http.StatusBadRequest)
+		return
+	}
+
+	pet := &entity.Pet{}
+	decoder := json.NewDecoder(r.Body)
+
+	err = decoder.Decode(pet)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	pet, err = c.petUsecase.UpdatePetByID(petID, pet)
+	if err != nil {
+		http.Error(w, `{"error": "Internal Server Error"}`, http.StatusInternalServerError)
+		return
+	}
+
+	response, err := json.Marshal(pet)
+	if err != nil {
+		http.Error(w, `{"error": "Internal Server Error"}`, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
 }
 
 func (c *petController) DeletePetByID(w http.ResponseWriter, r *http.Request) {
-	//TODO: path param
+	petID, err := strconv.ParseInt(chi.URLParam(r, "petID"), 10, 64)
+	if err != nil {
+		http.Error(w, `{"error": "Invalid petID format"}`, http.StatusBadRequest)
+		return
+	}
+
+	err = c.petUsecase.DeletePetByID(petID)
+	if err != nil {
+		http.Error(w, `{"error": "Internal Server Error"}`, http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
