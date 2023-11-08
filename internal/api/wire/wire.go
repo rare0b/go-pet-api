8eb47ff01e3f8c7afc42de11d312790ed83c95d3
rare0b <rare0b0110@gmail.com>
2023-11-09 00:51:32 +0900

    feat: wire追加

--- internal/api/wire/wire.go
+++ internal/api/wire/wire.go
@@ -3,21 +3,22 @@ package wire
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/google/wire"
+	"github.com/jmoiron/sqlx"
 	"github.com/rare0b/go-pet-api/internal/api/controller"
-	"github.com/rare0b/go-pet-api/internal/api/db"
 	"github.com/rare0b/go-pet-api/internal/api/repository"
 	"github.com/rare0b/go-pet-api/internal/api/router"
 	"github.com/rare0b/go-pet-api/internal/api/usecase"
 )
 
-func InitializeApp() *chi.Mux {
+func Wire(
+	db *sqlx.DB,
+) *chi.Mux {
 	wire.Build(
-		db.NewDB,
 		repository.NewPetRepository,
 		usecase.NewPetUsecase,
 		controller.NewPetController,
 		router.NewPetRouter,
 		router.NewMainRouter,
 	)
-	return nil
+	return &chi.Mux{}
 }
